6a0a215f154d8237d1bb5c212b388a294668aa74
lindsim <lindsey.simard@gmail.com>
2017-02-05 20:00:29 -0800

    function that draws tiles and phrases created

--- gameplay.js
+++ gameplay.js
@@ -1,41 +1,35 @@
-// if column is the same for length num of squares 
-//if row is same for length num of squares
-//if diagonal right to left
-//for x in range length: 
-// (x, x) 
-//(00, 11, 22, 33, 44)
-//for x in range length:
-//(length - x + 1), x 
-//if diagonal left to right
+function drawTiles() {
+  var canvas = document.getElementById('board');
+  if (canvas.getContext) {
+    var w = canvas.width;
+    var ctx = canvas.getContext('2d');
+    var space = 10;
+    var xCoord = space;
+    var yCoord = space;
+    var size = 3;
+    var textColor = "#000000";
+    var tileColor = "#ffffff";
+    var phrases = [
+      ["dog", "cat", "fish"],
+      ["pig", "turtle", "goblin"],
+      ["turkey", "child", "gecko"]
+    ];
+    var textPhrase = phrases[0][0];
+    //var textLen = 10;
+    var dem = (w - space * (size + 1)) / size;
+    for (var j = 0; j < size; j++) {
+      xCoord = space;
+      for (var i = 0; i < size; i++) {
+        textPhrase = phrases[j][i];
+        //textLen = ctx.measureText(textPrhase).width;
+        ctx.fillStyle = tileColor;
+        ctx.fillRect(xCoord, yCoord, dem, dem)
+        ctx.fillStyle = textColor;
+        ctx.fillText(textPhrase, xCoord + space, yCoord + (0.5 * dem))
+        xCoord += (dem + space)
+      }
+      yCoord += (dem + space)
+    }
 
-//generate board with default length squares
-//set middle square to free
-
-
-//new board when length changes
-//new board when new board button length selected -- are you sure if current board is in play
-
-//choose square color
-//choose stamp color
-//choose stamp shape
-
-
-//stamp - click to stamp
-//click again to erase stamp
-
-//check for winning by checking adjacent stamps in row until one is not stamped
-//check for winning by checking stamps above/below until one is not stamped
-//check of stamps in diagonal sets until one is not stamped
-
-//how to store stamp/not stamp value to elminate extra comparisons - r
-//start with 0, 0 as current check point
-//then go to 0, 1 etc
-// start with 0, 0, go to 1, 0 
-//get row of values; if false in row, return
-//if no false in row, return BINGO
-//get column of values; if false in column, return
-//if no false in column, return BINGO
-//check diagonal 1
-//check diagonal 2
-//if checking for matches, needs to check length * 2 + 2 groups (5 rows, 5 columns, 2 diagonals)
-
+  }
+}
