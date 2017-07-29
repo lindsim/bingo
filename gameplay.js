0770288bbba63473f3005388d05cffd7cf20576f
lindsim <lindsey.simard@gmail.com>
2017-07-28 18:49:11 -0700

    bingo working but not very elegantly

--- gameplay.js
+++ gameplay.js
@@ -1,35 +1,73 @@
 function newGame() {
   //get input if needbe to assign these variables
-  //start game
   //if current size == past size, no need to call makeBoard();
   var size = 3;
   var sizesq = size * size;
   var phrases_file = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  var bingos = [];
+  var needed_squares = [];
+  var marked = new Set();
   fillBoard();
 
   function fillBoard(){
-    if (document.getElementsByClassName('button-square').length !== sizesq){
+    if (document.getElementsByClassName('button-square').length < 1){
       createSquares();
+      makeBingos();
     } else {
       getNewPhrases();
     }
   }
-
-  function mark(){
-    alert('HOTDOG!');
+        
+  function makeBingos(){
+    for (var b = 0; b < size; b++){
+      var r_bingo = new Set();
+      var c_bingo = new Set();
+      for (var t = 0; t < size; t++){
+        r_bingo.add((b * size) + t);
+        c_bingo.add((t* size) + b); 
+      }
+    bingos.push(r_bingo);
+    bingos.push(c_bingo); 
+    }
+  
+   if (size % 2 !== 0){
+     var d0_bingo = new Set();
+     var d1_bingo = new Set();
+     for (var k = 0; k < size; k++){
+        d0_bingo.add(k * (size + 1)); 
+        d1_bingo.add((k + 1) * (size - 1)); 
+      }
+      bingos.push(d0_bingo);
+      bingos.push(d1_bingo);
+    }
   }
+
+  function mark(e){
+    var cur_btn = document.getElementById(e.target.id)
+    if (cur_btn.className === 'marked'){
+      cur_btn.className = 'button-square'; 
+      marked.delete(Number(cur_btn.id));
+    } else {
+      cur_btn.className = 'marked';
+      marked.add(Number(cur_btn.id));
+      bingoCheck();
+    }
+  }
+  
 
   function createSquares() {
     var phrases = [...getIndexesRandom()];
 
     for (var x = 0; x < sizesq; x++){
-        var button     = document.createElement('button');
+        var button = document.createElement('button');
         button.className = 'button-square';
         button.id = x;
-        button.addEventListener('click', mark(), false);
-        button.innerHTML = phrases_file[phrases[x]];
+        button.textContent = phrases_file[phrases[x]];
         document.getElementById('firstboard').appendChild(button);
-    }
+        button.onclick = mark;
+        //remember, no parens beacause assigning function NOT calling it
+      }
+
   }
 
   function getPhrasesShuffle () {
@@ -46,22 +84,35 @@ function newGame() {
   function getNewPhrases() {
     var phrases = [...getIndexesRandom()];
       for (var i = 0; i < sizesq; i++){
-        document.getElementById(i).innerHTML = phrases_file[phrases[i]];
-        //also set all squares to unmarked
+        document.getElementById(i).textContent = phrases_file[phrases[i]];
+        document.getElementById(i).className = 'button-square';
       }
     }
 
 
   function bingoCheck() {
-
+    bingos.forEach(function(bingo_set){
+     if (marked.isSuperset(bingo_set) === true ){
+       gameWon();
+       return;
+     } else {
+       return
+     };
+    })
   }
 
 
   function gameWon() {
+    alert("BINGO!!!!!!!!!!!! You won!");
   
   }
 
-  function drawBingo() {
-
-  }
+  Set.prototype.isSuperset = function(subset) {
+    for (var elem of subset) {
+        if (!this.has(elem)) {
+            return false;
+        }
+    }
+    return true;
+}
 }
